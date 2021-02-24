465c9f62ee325f825c47d773201d0c101d088b51
amir <amir.movafagh@gmail.com>
2021-02-25 00:43:43 +0330

    checkPoint

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     re_path(r'(?P<id>[0-9]+)/(?P<slug>[\w-]+)/', views.index, name='product_details'),
     path('addcomment/<int:id>/', views.add_comment, name='add_comment'),
+    path('ajaxcolor/', views.ajaxcolor, name='ajaxcolor'),
 
 ]
