74f6831d552a74cf1f2acba5c177ea33c9aa8070
amir <amir.movafagh@gmail.com>
2021-02-27 15:06:35 +0330

    checkPoint

--- home/templatetags/custom_tags.py
+++ home/templatetags/custom_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from order.models import ShopCart
 from product.models import Category
 
 register = template.Library()
@@ -11,6 +12,12 @@ def persian_int(english_int):
     number = str(english_int)
     return ''.join(devanagari_nums[int(digit)] for digit in number)
 
+
 @register.simple_tag
 def categorylist():
     return Category.objects.all()
+
+
+@register.simple_tag
+def shopcart_count(userid):
+    return ShopCart.objects.count(user_id=userid)
