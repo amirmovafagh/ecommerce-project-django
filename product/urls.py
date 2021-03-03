0999efd122ae350b3a47f2ec07fd19e229502428
amir <amir.movafagh@gmail.com>
2021-03-03 23:50:05 +0330

    workOn dynamic urls

--- product/urls.py
+++ product/urls.py
@@ -2,9 +2,10 @@ from django.urls import path, re_path
 
 from . import views
 
+app_name = 'product'
 urlpatterns = [
-    re_path(r'(?P<id>\d+)/(?P<slug>[-\w]+)/', views.index, name='product_details'),
-    path('addcomment/<int:id>/', views.add_comment, name='add_comment'),
-    path('ajaxcolor/', views.ajaxcolor, name='ajaxcolor'),
+    re_path(r'(?P<id>\d+)/(?P<slug>[-\w]+)/', views.index, name="product_details"),
+    path('addcomment/<int:id>/', views.add_comment, name="add_comment"),
+    path('ajaxcolor/', views.ajaxcolor, name="ajaxcolor"),
 
 ]
--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "user"
 urlpatterns = [
     path('', views.index, name='add_comment'),
     path('login', views.login_form, name='login'),
