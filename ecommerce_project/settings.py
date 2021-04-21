c3c07b593d9a79b43d9726c06371c55f2b2fff16
amir <amir.movafagh@gmail.com>
2021-04-21 13:06:18 +0430

    collectStatic

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -17,7 +17,6 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = "home:index"
 LOGOUT_REDIRECT_URL = "login"
@@ -136,11 +135,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -13,8 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 
@@ -44,8 +43,8 @@ urlpatterns = [path('', include('home.urls')),
                        name='category_products'),
                re_path(r'category/(?P<slug>[-\w]+)/(?P<page>\d+)/', views.CategoryProductsList.as_view(),
                        name='category_products'),
-               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # for showing images
-
+               ]
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # for showing images
 # admin.site.site_header = 'Admin'
 # admin.site.site_title = 'Administration'
 # admin.site.index_title = 'Welcome To Administration'
